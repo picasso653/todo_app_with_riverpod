3866c11df6725468eb7e0b8cce2aec599e4cfa40
Isaac Korankye Asante <xquisit31@gmail.com>
2024-08-03 07:13:52 +0000

    New flutter project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'features/on_boarding/views/on_boarding_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ToDo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const OnBoardingScreen(),
+    );
+  }
+}
+
+
